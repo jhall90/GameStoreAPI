10b4679361bb294856ede63dca602449fc46356c
Josh Hall <hall.joshua.220@gmail.com>
2024-04-22 16:12:18 -0600

    added an auto migrate on start up so if the table is deleted it will be readded without having to run the migrate commands manually

---
+++ GameStore.API/Data/DataExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GameStore.API.Data;
+
+public static class DataExtensions
+{
+    // must be static because it is a extension method using "this"
+    /// <summary>
+    /// Execute Migration on start-up
+    /// </summary>
+    /// <param name="app"></param>
+    public static void MigrateDb(this WebApplication app)
+    {
+        using var scope = app.Services.CreateScope();
+        // because we did builder.Services.AddSqlite<GameStoreContext>(connString); 
+        //      in the Program.cs the program knows about the context so we can call an instance here
+        var dbContext = scope.ServiceProvider.GetRequiredService<GameStoreContext>();
+        dbContext.Database.Migrate();
+    }
+}
