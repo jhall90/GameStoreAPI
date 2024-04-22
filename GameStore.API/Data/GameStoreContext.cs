f6c8f2288ed6fb24743bd18d1e4a1535aaf501aa
Josh Hall <hall.joshua.220@gmail.com>
2024-04-22 16:26:06 -0600

    added SeedGenre migration to create some genres in the database - this will run when Data extension app.MigrateDb(); runs

--- GameStore.API/Data/GameStoreContext.cs
+++ GameStore.API/Data/GameStoreContext.cs
@@ -16,4 +16,26 @@ public class GameStoreContext(DbContextOptions<GameStoreContext> options)
     //      passing in the the correct type 
     public DbSet<Game> Games => Set<Game>();
     public DbSet<Genre> Genres => Set<Genre>();
+
+    /// <summary>
+    /// Executed with the migration execution
+    /// Only for simple data, don't use when the data is complex
+    /// this doesn't need to interact with any other areas in the application so it is ok to do this for now
+    /// only static data
+    /// </summary>
+    /// <param name="modelBuilder"></param>
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Genre>().HasData(
+            new { Id = 1, Name = "MMORPG" },
+            new { Id = 2, Name = "Fighting" },
+            new { Id = 3, Name = "Sports" },
+            new { Id = 4, Name = "Racing" },
+            new { Id = 5, Name = "RPG" },
+            new { Id = 6, Name = "Kids and Family" },
+            new { Id = 7, Name = "Horror" },
+            new { Id = 8, Name = "Simulation" },
+            new { Id = 9, Name = "Shooter" }
+        );
+    }
 }
---
+++ GameStore.API/Data/Migrations/20240422221917_SeedGenres.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace GameStore.API.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedGenres : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Genres",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "MMORPG" },
+                    { 2, "Fighting" },
+                    { 3, "Sports" },
+                    { 4, "Racing" },
+                    { 5, "RPG" },
+                    { 6, "Kids and Family" },
+                    { 7, "Horror" },
+                    { 8, "Simulation" },
+                    { 9, "Shooter" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Genres",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Genres",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Genres",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Genres",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Genres",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Genres",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Genres",
+                keyColumn: "Id",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "Genres",
+                keyColumn: "Id",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "Genres",
+                keyColumn: "Id",
+                keyValue: 9);
+        }
+    }
+}
