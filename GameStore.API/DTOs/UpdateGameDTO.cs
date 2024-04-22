c7d6e3a06da94d5361e4cdfe32a91658e4b0c278
Josh Hall <hall.joshua.220@gmail.com>
2024-04-22 15:51:14 -0600

    missed some staged changes

---
+++ GameStore.API/DTOs/UpdateGameDTO.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GameStore.API.DTOs;
+
+public record class UpdateGameDTO(
+    [Required][StringLength(50)] string Name,
+    [Required][StringLength(40)] string Genre,
+    [Required][Range(1, 100)] decimal Price,
+    DateOnly ReleaseDate
+);
